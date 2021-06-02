5e9646b71b91fbea7695067db5708f030a622bc2
AveryanovSS <mr.averyanov.s@gmail.com>
2021-06-02 22:13:28 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by  Sergey on 02.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
